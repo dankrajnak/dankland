c9dfd249d329c3cb85d82ae79b91109ac30f8bab
Daniel Krajnak <dan.krajnak@gmail.com>
2020-05-11 23:49:57 -0400

    Test deploy summary

--- src/pages/again.tsx
+++ src/pages/again.tsx
@@ -66,7 +66,7 @@ const IndexPage = (): React.ReactNode => {
       {currentPage === 0 && (
         <Fader visible={currentPage === 0}>
           <TitleHolder>
-            <Title color="#EEE">Again, again, again</Title>
+            <Title color="#EEE">Change what this says</Title>
           </TitleHolder>
           <Lorenz width={width} height={height} />
         </Fader>
