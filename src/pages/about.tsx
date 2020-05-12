785942f543e7deec263b1b9cd6cd312930626725
Daniel Krajnak <dan.krajnak@gmail.com>
2020-05-11 23:42:35 -0400

    Remove webassembly button

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -17,42 +17,21 @@ const Content = styled.div`
   line-height: 1.8rem;
 `;
 
-const About = () => {
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  const greet = React.useRef<() => void>(() => {});
-
-  React.useEffect(() => {
-    import(`../webassembly/fluid/pkg`)
-      .then((module) => {
-        console.log("LOADED MODULE");
-        greet.current = module.greet;
-      })
-      .catch(console.error);
-  }, []);
-  return (
-    <MenuLayout color="black">
-      <SEO title="About" />
-      <Container>
-        <Content>
-          <h4>This is just stuff I like making.</h4>
-          <p>
-            My name's Daniel. This is a place that I put stuff I make. If you
-            like my work, feel free to reach out. My email is the first three
-            letters of my first name plus a '.' plus my last name (krajnak).
-            It's the google kind. Suck on that, email-harvesting bots.
-          </p>
-          <button
-            onClick={() => {
-              console.log("click");
-              greet.current();
-            }}
-          >
-            I'm a web-assembly button!
-          </button>
-        </Content>
-      </Container>
-    </MenuLayout>
-  );
-};
+const About = () => (
+  <MenuLayout color="black">
+    <SEO title="About" />
+    <Container>
+      <Content>
+        <h4>This is just stuff I like making.</h4>
+        <p>
+          My name's Daniel. This is a place that I put stuff I make. If you like
+          my work, feel free to reach out. My email is the first three letters
+          of my first name plus a '.' plus my last name (krajnak). It's the
+          google kind. Suck on that, email-harvesting bots.
+        </p>
+      </Content>
+    </Container>
+  </MenuLayout>
+);
 
 export default About;
