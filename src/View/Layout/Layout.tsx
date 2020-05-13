c59b2985ecf52b34971acf44bbb9c08ffe503bd2
Daniel Krajnak <dan.krajnak@gmail.com>
2020-05-13 19:35:59 -0400

    Implement temp google analytics integration

--- src/View/Layout/Layout.tsx
+++ src/View/Layout/Layout.tsx
@@ -1,17 +1,32 @@
 import * as React from "react";
 import globalStyles from "../Styles/global";
+import GoogleAnalyticsService from "../../Services/GoogleAnalytics/GoogleAnalytics.service";
+import useSafeWindow from "../Hooks/useSafeWindow";
 
 interface Props {
   children: React.ReactNode;
 }
+// This is kinda hacky.  There's a ga plugin that next is developing.  This will hold us over until then.
+let gaInitialized = false;
 
-const Layout = (props: Props) => (
-  <div>
-    {props.children}
-    <style jsx global>
-      {globalStyles}
-    </style>
-  </div>
-);
+const Layout = (props: Props) => {
+  const [safeWindow] = useSafeWindow();
+  React.useEffect(() => {
+    if (safeWindow && !gaInitialized) {
+      GoogleAnalyticsService.initGA();
+      gaInitialized = true;
+    }
+    GoogleAnalyticsService.logPageView();
+  }, [safeWindow]);
+
+  return (
+    <div>
+      {props.children}
+      <style jsx global>
+        {globalStyles}
+      </style>
+    </div>
+  );
+};
 
 export default Layout;
