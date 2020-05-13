b717eab0bf633e5fcf1778359cfebaf51fe20a98
Daniel Krajnak <dan.krajnak@gmail.com>
2020-05-13 19:03:36 -0400

    Convert seo to use next/head for ssr

--- src/View/Utility/seo.tsx
+++ src/View/Utility/seo.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
-import { Helmet } from "react-helmet";
+import Head from "next/head";
 
 const siteTitle = "Dank Land";
-const metaDescription = "Where this one guy puts stuff he codes";
+const defaultDescription = "The thing one guy works on over his weekends";
 const defaultKeywords = [
   "art",
   "dank",
@@ -16,83 +16,76 @@ const defaultKeywords = [
 interface Props {
   description?: string;
   lang?: string;
-  meta?: [];
+  meta?: { name: string; content: string; property?: undefined }[];
   keywords?: string[];
   title: string;
 }
 
-// Even though nextJS has its own head component.  It has some drawbacks.
-// Namely, you can't specify a language attribute on the html element
-// and it seems like there may be problems with the upcoming concurrent mode.
-// I'm going to keep this in react-helmet for now.
-
 const SEO = ({
-  description = metaDescription,
-  lang = "en",
+  description = defaultDescription,
   meta = [],
   keywords = defaultKeywords,
   title,
-}: Props) => (
-  <Helmet
-    htmlAttributes={{
-      lang,
-    }}
-    title={title}
-    titleTemplate={`%s | ${siteTitle}`}
-    meta={[
-      {
-        name: `description`,
-        content: metaDescription,
-      },
-      {
-        property: `og:title`,
-        content: title,
-      },
-      {
-        property: `og:description`,
-        content: description,
-      },
-      {
-        property: `og:type`,
-        content: `website`,
-      },
-      {
-        name: `twitter:card`,
-        content: `summary`,
-      },
-      {
-        name: `twitter:title`,
-        content: title,
-      },
-      {
-        name: `twitter:description`,
-        content: metaDescription,
-      },
-      { name: `theme-color`, content: "#eee" },
-    ]
-      .concat(
-        keywords.length > 0
-          ? {
-              name: `keywords`,
-              content: keywords.join(`, `),
-            }
-          : []
-      )
-      .concat(meta)}
-  >
-    <link rel="manifest" href="/manifest.json" />
-    <link rel="icon" href="/favicon-32x32.png" />
-    <link
-      rel="apple-touch-icon"
-      sizes="192x192"
-      href="android-chrome-192x192.png"
-    />
-    <link
-      rel="apple-touch-icon"
-      sizes="512x512"
-      href="android-chrome-512x512.png"
-    />
-  </Helmet>
-);
+}: Props) => {
+  const defaultMeta = [
+    {
+      name: `description`,
+      content: description,
+    },
+    {
+      property: `og:title`,
+      content: siteTitle,
+    },
+    {
+      property: `og:description`,
+      content: description,
+    },
+    {
+      property: `og:type`,
+      content: `website`,
+    },
+    {
+      name: `twitter:card`,
+      content: `summary`,
+    },
+    {
+      name: `twitter:creator`,
+      content: "Daniel Krajnak",
+    },
+    {
+      name: `twitter:title`,
+      content: "DankLand",
+    },
+    {
+      name: `twitter:description`,
+      content: description,
+    },
+    { name: `theme-color`, content: "#eee" },
+    { name: `og:image`, content: "/android-chrome-512x512.png" },
+    { name: "keywords", content: keywords.join(", ") },
+  ];
+  return (
+    <Head>
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="icon" href="/favicon-32x32.png" />
+      <link
+        rel="apple-touch-icon"
+        sizes="192x192"
+        href="/android-chrome-192x192.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="512x512"
+        href="/android-chrome-512x512.png"
+      />
+      <title>
+        {title} | {siteTitle}
+      </title>
+      {[...meta, ...defaultMeta].map((info, key) => (
+        <meta {...info} key={key} />
+      ))}
+    </Head>
+  );
+};
 
 export default SEO;
