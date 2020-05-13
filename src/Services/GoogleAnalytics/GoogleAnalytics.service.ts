acf6ab19d4ab8ef2c54be3e5f7ba6a2f30dbb9e6
Daniel Krajnak <dan.krajnak@gmail.com>
2020-05-13 19:39:30 -0400

    Fix build

---
+++ src/Services/GoogleAnalytics/GoogleAnalytics.service.ts
@@ -0,0 +1,37 @@
+// utils/analytics.js
+import ReactGA from "react-ga";
+
+// Janky, but will get the job done for now.
+const hostNameFilter = "dank";
+const atDankLand = (): boolean => window.location.hostname === hostNameFilter;
+
+export default class GoogleAnalyticsService {
+  static initGA() {
+    if (atDankLand()) {
+      ReactGA.initialize("UA-108070651-2");
+    }
+  }
+
+  static logPageView() {
+    if (atDankLand()) {
+      ReactGA.set({ page: window.location.pathname });
+      ReactGA.pageview(window.location.pathname);
+    }
+  }
+
+  static logEvent(category = "", action = "") {
+    if (atDankLand()) {
+      if (category && action) {
+        ReactGA.event({ category, action });
+      }
+    }
+  }
+
+  static logException(description = "", fatal = false) {
+    if (atDankLand()) {
+      if (description) {
+        ReactGA.exception({ description, fatal });
+      }
+    }
+  }
+}
