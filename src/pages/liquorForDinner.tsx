361940794c864e18a9e4f986ad238d7d731d3e44
Daniel Krajnak <dan.krajnak@gmail.com>
2020-07-08 22:43:52 -0400

    It's all I could think of

--- src/pages/liquorForDinner.tsx
+++ src/pages/liquorForDinner.tsx
@@ -2,11 +2,26 @@ import * as React from "react";
 import MenuLayout from "../View/Layout/MenuLayout";
 import SEO from "../View/Utility/seo";
 
+const CIRCLE_RADIUS = 30;
+const STROKE_WIDTH = 1;
 const LiquorForDinner = () => (
   <MenuLayout>
     <SEO title="Liquor For Dinner" />
-    <div className="container">
-      <h2 className="content">Nothing's Here.</h2>
+    <div className="container" title="it's all I could think of">
+      <h2 className="content">
+        <svg
+          width={(CIRCLE_RADIUS + STROKE_WIDTH) * 2}
+          height={(CIRCLE_RADIUS + STROKE_WIDTH) * 2}
+        >
+          <circle
+            cx={CIRCLE_RADIUS + STROKE_WIDTH}
+            cy={CIRCLE_RADIUS + STROKE_WIDTH}
+            r={CIRCLE_RADIUS}
+            stroke="white"
+            strokeWidth={STROKE_WIDTH}
+          />
+        </svg>
+      </h2>
     </div>
     <style jsx>
       {`
