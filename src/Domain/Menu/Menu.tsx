d03154876287cac53c2b3d597e3eba3450abdc46
Daniel Krajnak <dan.krajnak@gmail.com>
2020-07-30 00:31:25 -0400

    Upgrade to latest next version

--- src/Domain/Menu/Menu.tsx
+++ src/Domain/Menu/Menu.tsx
@@ -1,9 +1,6 @@
 import * as React from "react";
 import Card from "../Card/Card";
 
-type PropsPassedThroughLocationState = {
-  prevPath: string | null;
-};
 export type MenuRouteProps = any;
 
 export type Menu = React.FunctionComponent<{
