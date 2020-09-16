07918db49a8c8ea71b3e5934beccb268d874bef2
Daniel Krajnak <dan.krajnak@gmail.com>
2020-09-16 00:27:09 -0400

    Add sentry

---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { AppProps } from "next/app";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+if (process.env.SENTRY_PUBLIC_DSN) {
+  console.log(process.env.SENTRY_PUBLIC_DSN);
+  Sentry.init({
+    enabled: process.env.NODE_ENV === "production",
+    dsn: process.env.SENTRY_PUBLIC_DSN,
+    integrations: [new Integrations.BrowserTracing()],
+    tracesSampleRate: 1.0,
+  });
+}
+
+const App: React.FunctionComponent<AppProps> = ({ Component, pageProps }) => (
+  <Component {...pageProps} />
+);
+
+export default App;
