1f7d29d3c139ede74f737367e96465cfed558373
Daniel Krajnak <dan.krajnak@gmail.com>
2020-10-18 12:42:58 -0400

    Add web vitals to google analytics

--- src/Services/GoogleAnalytics/GoogleAnalytics.service.ts
+++ src/Services/GoogleAnalytics/GoogleAnalytics.service.ts
@@ -1,33 +1,31 @@
 // utils/analytics.js
-import ReactGA from "react-ga";
+import ReactGA, { EventArgs } from "react-ga";
 
 // Janky, but will get the job done for now.
 const hostNameFilter = "dank.land";
 const atDankLand = (): boolean => window.location.hostname === hostNameFilter;
 
 export default class GoogleAnalyticsService {
-  static initGA() {
+  static initGA(): void {
     if (atDankLand()) {
       ReactGA.initialize("UA-108070651-2");
     }
   }
 
-  static logPageView() {
+  static logPageView(): void {
     if (atDankLand()) {
       ReactGA.set({ page: window.location.pathname });
       ReactGA.pageview(window.location.pathname);
     }
   }
 
-  static logEvent(category = "", action = "") {
+  static logEvent(args: EventArgs): void {
     if (atDankLand()) {
-      if (category && action) {
-        ReactGA.event({ category, action });
-      }
+      ReactGA.event(args);
     }
   }
 
-  static logException(description = "", fatal = false) {
+  static logException(description = "", fatal = false): void {
     if (atDankLand()) {
       if (description) {
         ReactGA.exception({ description, fatal });
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
 import { FunctionComponent } from "react";
-import { AppProps } from "next/app";
+import { AppProps, NextWebVitalsMetric } from "next/app";
 import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing";
+import GoogleAnalyticsService from "../Services/GoogleAnalytics/GoogleAnalytics.service";
 
 if (process.env.SENTRY_PUBLIC_DSN) {
   Sentry.init({
@@ -14,5 +15,20 @@ if (process.env.SENTRY_PUBLIC_DSN) {
 const App: FunctionComponent<AppProps> = ({ Component, pageProps }) => (
   <Component {...pageProps} />
 );
+
+export function reportWebVitals({
+  id,
+  name,
+  label,
+  value,
+}: NextWebVitalsMetric) {
+  GoogleAnalyticsService.logEvent({
+    action: "Metrics",
+    category: label === "web-vital" ? "Web Vitals" : "Next.js custom metric",
+    value: Math.round(name === "CLS" ? value * 1000 : value), // values must be integers
+    label: id, // id unique to current page load
+    nonInteraction: true, // avoids affecting bounce rate.
+  });
+}
 
 export default App;
