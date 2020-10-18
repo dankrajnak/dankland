b2ffd6ff72fa5df8493fe3e80ea37da94e4c8a09
Daniel Krajnak <dan.krajnak@gmail.com>
2020-10-18 14:08:09 -0400

    Use bugsnag instead of sentry

--- next.config.js
+++ next.config.js
@@ -1,32 +1,14 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 // next.config.js
 const withOffline = require("next-offline");
-// const nextConfig = {
-//   target: "serverless",
-//   transformManifest: (manifest) => ["/"].concat(manifest), // add the homepage to the cache
-//   // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
-//   // turn on the SW in dev mode so that we can actually test it
-//   generateInDevMode: false,
-//   workboxOpts: {
-//     swDest: "static/service-worker.js",
-//     runtimeCaching: [
-//       {
-//         urlPattern: /^https?.*/,
-//         handler: "NetworkFirst",
-//         options: {
-//           cacheName: "https-calls",
-//           networkTimeoutSeconds: 15,
-//           expiration: {
-//             maxEntries: 150,
-//             maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
-//           },
-//           cacheableResponse: {
-//             statuses: [0, 200],
-//           },
-//         },
-//       },
-//     ],
-//   },
-// };
 
-module.exports = withOffline({});
+module.exports = withOffline({
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    BUGSNAG_API_KEY: process.env.BUGSNAG_SERVER_API_KEY,
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    BUGSNAG_API_KEY: process.env.BUGSNAG_BROWSER_API_KEY, // Pass through env variables
+  },
+});
