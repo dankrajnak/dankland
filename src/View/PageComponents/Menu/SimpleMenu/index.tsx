af1f9662546b56f8e9e69b54d004b463c885d1b0
Daniel Krajnak <dan.krajnak@gmail.com>
2021-01-28 00:50:52 -0500

    Fix key on simple menu

--- src/View/PageComponents/Menu/SimpleMenu/index.tsx
+++ src/View/PageComponents/Menu/SimpleMenu/index.tsx
@@ -6,8 +6,8 @@ const SimpleMenu: Menu = ({ cards }) => (
   <>
     <div className="cards-holder">
       {cards.map((card, index) => (
-        <div className="card-holder">
-          <Link href={card.link} key={index}>
+        <div className="card-holder" key={index}>
+          <Link href={card.link}>
             <a className="card-graphic">
               <card.background width={300} height={300} />
             </a>
