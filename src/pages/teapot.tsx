7684ad3fa142e9c177ece4c9b36fa292793991c4
Daniel Krajnak <dan.krajnak@gmail.com>
2021-09-15 12:39:26 -0400

    Improve tea page

--- src/pages/teapot.tsx
+++ src/pages/teapot.tsx
@@ -1,18 +1,59 @@
+import useAsync from "@danielkrajnak/use-async";
 import MenuLayout from "../View/Layout/MenuLayout";
 
-const getTeapot = async () => {
-  try {
-    const response = await fetch("/api/teapot");
-    return response.json();
-  } catch (e) {
-    return e;
-  }
+const getTeapot = async (): Promise<Record<string, unknown>> => {
+  const response = await fetch("/api/teapot");
+  return {
+    status: response.status,
+    statusText: response.statusText,
+    body: await response.json(),
+  };
 };
 
-const Teapot = () => (
-  <MenuLayout>
-    <button onClick={() => getTeapot()}>Make coffee</button>
-  </MenuLayout>
-);
+const Teapot = () => {
+  const [state, makeCoffee] = useAsync(getTeapot);
+  let text = "{}";
+  if (state.isLoading) {
+    text = "Loading ...";
+  } else if (state.errorMessage) {
+    text =
+      typeof state.errorMessage === "string"
+        ? state.errorMessage
+        : "There was an error :(";
+  } else if (state.data) {
+    text = JSON.stringify(state.data, null, 2);
+  }
+  return (
+    <MenuLayout>
+      <div className="container">
+        <button onClick={() => makeCoffee()}>Make coffee</button>
+        <pre>{text}</pre>
+      </div>
+      <style jsx>
+        {`
+          .container {
+            width: 50%;
+            margin: 20px auto;
+          }
+
+          @media screen and (max-width: 600px) {
+            .container {
+              width: 90%;
+            }
+          }
+
+          pre {
+            overflow-x: auto;
+            font-family: monospace;
+            background-color: #efefef;
+            padding: 5px;
+            border: solid 1px #aaa;
+            border-radius: 3px;
+          }
+        `}
+      </style>
+    </MenuLayout>
+  );
+};
 
 export default Teapot;
