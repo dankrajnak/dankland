22b097cc2f1d1c93b748619652575a0103cbb39c
Dan <dan.krajnak@gmail.com>
2022-01-27 21:01:35 -0500

    Optimize font load

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,7 +15,12 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Outfit:wght@100;400&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
