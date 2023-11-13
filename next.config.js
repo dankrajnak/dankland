48258d14a0e076c1105de11ecfc8fd166530781b
Dan <dan.krajnak@gmail.com>
2023-11-12 23:58:04 -0500

    Fix scroll restoration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 // next.config.js
 // const withOffline = require("next-offline");
 
+/** @type {import('next').NextConfig} */
 module.exports = {
   serverRuntimeConfig: {
     // Will only be available on the server side
@@ -12,6 +13,10 @@ module.exports = {
     BUGSNAG_API_KEY: process.env.BUGSNAG_BROWSER_API_KEY, // Pass through env variables
     FAUNA_DB: process.env.FAUNA_DB,
   },
+  experimental: {
+    scrollRestoration: true,
+  },
+
   webpack: (config) => ({
     ...config,
     experiments: {
