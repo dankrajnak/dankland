b4449c8884398024461923400dc011bdce8090f8
Dan <dan.krajnak@gmail.com>
2024-05-11 15:15:22 +0400

    Add vercel speed insights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,7 @@ import { FunctionComponent } from "react";
 import { AppProps, NextWebVitalsMetric, AppContext } from "next/app";
 import Bugsnag from "../Services/Bugsnag/Bugsnag.service";
 import "../View/Styles/global.css";
-
-import GoogleAnalyticsService from "../Services/GoogleAnalytics/GoogleAnalytics.service";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const ErrorBoundary = Bugsnag.getPlugin("react") as any;
 
@@ -17,23 +16,9 @@ export async function getInitialProps({ Component, ctx }: AppContext) {
   return { pageProps };
 }
 
-export function reportWebVitals({
-  id,
-  name,
-  label,
-  value,
-}: NextWebVitalsMetric) {
-  GoogleAnalyticsService.logEvent({
-    action: name,
-    category: label === "web-vital" ? "Web Vitals" : "Next.js custom metric",
-    value: Math.round(name === "CLS" ? value * 1000 : value), // values must be integers
-    label: id, // id unique to current page load
-    nonInteraction: true, // avoids affecting bounce rate.
-  });
-}
-
 const App: FunctionComponent<AppProps> = ({ Component, pageProps }) => (
   <ErrorBoundary FallbackComponent={Error}>
+    <SpeedInsights />
     <Component {...pageProps} />
   </ErrorBoundary>
 );
